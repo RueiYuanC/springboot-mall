858d7ba0c2216c474f504827da09d3f6f22e1b74
Slow <slowmocross@gmail.com>
2023-12-27 23:03:23 +0800

    帳號功能-檢查註冊的email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/slow/springbootmall/dao/UserDao.java
+++ springboot-mall/src/main/java/com/slow/springbootmall/dao/UserDao.java
@@ -4,6 +4,7 @@ import com.slow.springbootmall.dto.UserRegisterRequest;
 import com.slow.springbootmall.model.User;
 
 public interface UserDao {
+    User getUserByEmail(String email);
     User getUserById(Integer userId);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
--- springboot-mall/src/main/java/com/slow/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/slow/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,20 @@ import com.slow.springbootmall.dao.UserDao;
 import com.slow.springbootmall.dto.UserRegisterRequest;
 import com.slow.springbootmall.model.User;
 import com.slow.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+
 
 @Component
 public class UserServiceImpl implements UserService {
+
+   private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +28,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        //檢查MAIL
+        User user=userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("該 email {} 已經被註冊",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
