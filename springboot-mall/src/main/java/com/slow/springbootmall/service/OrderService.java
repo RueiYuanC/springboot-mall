732cf98f139cb9c6748296bf4c318a317e736bb1
Slow <slowmocross@gmail.com>
2024-01-09 22:05:21 +0800

    訂單功能-查詢訂單列表

--- springboot-mall/src/main/java/com/slow/springbootmall/service/OrderService.java
+++ springboot-mall/src/main/java/com/slow/springbootmall/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.slow.springbootmall.service;
 
 import com.slow.springbootmall.dto.CreateOrderRequest;
+import com.slow.springbootmall.dto.OrderQueryParams;
 import com.slow.springbootmall.model.Order;
 
+import java.util.List;
+
 public interface OrderService {
+    Integer countOrder(OrderQueryParams orderQueryParams);
+    List<Order> getOrders(OrderQueryParams orderQueryParams);
     Order getOrderById(Integer orderId);
     Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest);
 }
--- springboot-mall/src/main/java/com/slow/springbootmall/service/impl/OrderServiceImpl.java
+++ springboot-mall/src/main/java/com/slow/springbootmall/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.slow.springbootmall.dao.ProductDao;
 import com.slow.springbootmall.dao.UserDao;
 import com.slow.springbootmall.dto.BuyItem;
 import com.slow.springbootmall.dto.CreateOrderRequest;
+import com.slow.springbootmall.dto.OrderQueryParams;
 import com.slow.springbootmall.model.Order;
 import com.slow.springbootmall.model.OrderItem;
 import com.slow.springbootmall.model.Product;
@@ -32,6 +33,22 @@ public class OrderServiceImpl implements OrderService {
     private ProductDao productDao;
 
     private UserDao userDao;
+
+    @Override
+    public Integer countOrder(OrderQueryParams orderQueryParams) {
+        return orderDao.countOrder(orderQueryParams);
+    }
+
+    @Override
+    public List<Order> getOrders(OrderQueryParams orderQueryParams) {
+        List<Order> orderList= orderDao.getOrders(orderQueryParams);
+        for (Order order : orderList){
+            List<OrderItem> orderItemList = orderDao.getOrderItemsByOrderId(order.getOrderId());
+            order.setOrderItemList(orderItemList);
+        }
+
+        return orderList;
+    }
 
     @Override
     public Order getOrderById(Integer orderId) {
